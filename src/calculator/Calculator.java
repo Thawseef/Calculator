4f4394b3ba0b7063b8160a04a356c8a7e6354d3d
thawseef <thawseef@acer>
2019-01-22 17:34:44 +0530

    Calculator

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author thawseef
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      int a = calc.add(25, 25);
+      int b = calc.devide(25, 5);
+      int c = calc.multiply(25, 25);
+      int d = calc.subtrack(25, 10);
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+    }
+    
+}
---
+++ src/calculator/calc.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author thawseef
+ */
+public class calc {
+        
+    public static int add (int firstnum1,int seconednum2){ 
+        return firstnum1+seconednum2;
+    }
+    public static int subtrack (int firstnum1,int seconednum2){ 
+        return firstnum1-seconednum2;
+    }   
+    public static int devide (int firstnum1,int seconednum2){ 
+        return firstnum1/seconednum2;
+    }
+    public static int multiply (int firstnum1,int seconednum2){ 
+        return firstnum1*seconednum2;
+    }
+ 
+    
+    
+}
